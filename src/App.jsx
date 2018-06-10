f46f6b4ee7937ea6c680838df496c564f30934cd
Alexys Gonzalez <alexysbike@gmail.com>
2018-06-10 14:10:37 -0400

    Setting configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { Component } from 'react';
-import './App.css';
 
 export default class App extends Component {
   state = {
-    name: 'sophia-app'
+    name: 'sophia-app',
   };
 
   render() {
     return (
-      <div className="App">
+      <div className="container">
         <h1>Welcome to {this.state.name}</h1>
       </div>
     );
