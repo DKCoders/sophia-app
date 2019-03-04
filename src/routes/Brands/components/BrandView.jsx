465a521d8c537634138007d9c6f066550bce03cb
Alexys Gonzalez {DK} <alexysbike@gmail.com>
2019-03-04 01:23:33 -0300

    Moving brand view

--- src/routes/Brands/components/BrandView.jsx
+++ src/routes/Brands/components/BrandView.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 import { connect } from 'react-redux';
-import BasicView from '../../../../components/BasicView';
-import * as brandSelectors from '../../../../modules/brand/selectors';
+import BasicView from '../../../components/BasicView';
+import * as brandSelectors from '../../../modules/brand/selectors';
 
 const mapStateToProps = (state, { match: { params: { id } } }) => ({
   item: brandSelectors.brandById(state, { id }),
